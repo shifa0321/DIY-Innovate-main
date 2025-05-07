3a48d6362fb843ea4bf087a76d6c2f38310dce77
ayushman-111202 <ayushmanshukla.2021@outlook.com>
2025-05-07 19:45:05 +0530

    navbar updated

--- frontend/src/app/user/layout.jsx
+++ frontend/src/app/user/layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../../components/Navbar';
+import Navbar from './Navbar';
 import Footer from '@/components/Footer';
 import { Toaster } from 'react-hot-toast';
 
